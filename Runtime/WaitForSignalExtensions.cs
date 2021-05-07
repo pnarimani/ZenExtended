89adec3b85a7c9e943fd5f5e001683e0d50312ab
Peyman Narimani <pmnarimani@gmail.com>
2021-05-07 13:33:41 +0430

    Added wait for signal

---
+++ Runtime/WaitForSignalExtensions.cs
@@ -0,0 +1,24 @@
+#if USE_UNITASK
+
+using Cysharp.Threading.Tasks;
+using Zenject;
+
+namespace ZenExtended
+{
+    public static class WaitForSignalExtensions
+    {
+        public static UniTask<T> WaitForSignal<T>(this SignalBus signalBus)
+        {
+            var source = AutoResetUniTaskCompletionSource<T>.Create();
+            signalBus.Subscribe<T>(Received);
+            return source.Task;
+
+            void Received(T t)
+            {
+                signalBus.Unsubscribe<T>(Received);
+                source.TrySetResult(t);
+            }
+        }
+    }
+}
+#endif
