a5f35c03b6e6a0faa79bec865dc9fc2c9bed5953
Peyman Narimani <pmnarimani@gmail.com>
2021-05-17 12:38:20 +0430

    Saving rotation and scale in TransformInfo

--- Runtime/AnimatedPanel/TransformInfo.cs
+++ Runtime/AnimatedPanel/TransformInfo.cs
@@ -6,17 +6,22 @@ namespace ZenExtended
 {
     internal readonly struct TransformInfo
     {
-        public readonly Vector2 AnchorMin, AnchorMax, AnchoredPosition, SizeDelta;
+        private readonly Vector2 AnchorMin, AnchorMax, AnchoredPosition, SizeDelta;
+        private readonly Vector3 Scale;
+        private readonly Quaternion Rotation;
 
-        public TransformInfo(Vector2 anchorMin, Vector2 anchorMax, Vector2 anchoredPosition, Vector2 sizeDelta)
+
+        public TransformInfo(Vector2 anchorMin, Vector2 anchorMax, Vector2 anchoredPosition, Vector2 sizeDelta, Quaternion rotation, Vector3 scale)
         {
             AnchorMin = anchorMin;
             AnchorMax = anchorMax;
             AnchoredPosition = anchoredPosition;
             SizeDelta = sizeDelta;
+            Rotation = rotation;
+            Scale = scale;
         }
 
-        public TransformInfo(RectTransform t) : this(t.anchorMin, t.anchorMax, t.anchoredPosition, t.sizeDelta)
+        public TransformInfo(RectTransform t) : this(t.anchorMin, t.anchorMax, t.anchoredPosition, t.sizeDelta, t.rotation, t.localScale)
         {
         }
 
@@ -26,6 +31,8 @@ namespace ZenExtended
             t.anchorMax = AnchorMax;
             t.anchoredPosition = AnchoredPosition;
             t.sizeDelta = SizeDelta;
+            t.rotation = Rotation;
+            t.localScale = Scale;
         }
     }
 }
